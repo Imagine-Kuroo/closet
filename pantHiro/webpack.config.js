a2d148b99ad54a183b476429b3a7c87fa1c318bd
peggy <maggiegu94@163.com>
2018-07-27 10:31:28 +0800

    配置了热加载....但在此之前报错了....

--- pantHiro/webpack.config.js
+++ pantHiro/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader')
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader');
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
     entry: path.join(__dirname, 'src/index.js'),
     module: {
         rules: [
@@ -43,6 +47,33 @@ module.exports = {
         path: path.join(__dirname, 'dist')
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new HTMLPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        })
     ]
 }
+
+if (isDev) {
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true,
+        },
+        hot: true,
+        // historyFallback: {
+        // },
+        // open: true
+    },
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config

7e6b0b5c3e13ae5a6c7a787e21f9d62454f87e39
peggy <maggiegu94@163.com>
2018-09-06 13:41:33 +0800

    增加 webpack.config.practice.js 专门作为学vue API的配置文件

---
+++ pantVueSSR/build/webpack.config.practice.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const devServer = {
+  port: 8088,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true,
+  },
+  hot: true
+}
+
+let config
+
+if (isDev) {
+  config = merge(baseConfig, {
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    devServer,
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ]
+  })
+} else {
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, 'src/index.js'),
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [{
+        test: /\.styl/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      }]
+    },
+    plugins: {
+      // new MiniCssExtractPlugin({
+      //   filename: 'style.[contentHash:8].css'
+      // })
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            chunks: 'initial',
+            minChunks: 2, maxInitialRequests: 5,
+            minSize: 0
+          },
+          vendor: {
+            test: /node_modules/,
+            chunks: 'initial',
+            name: 'vendor',
+            priority: 10,
+            enforce: true
+          }
+        }
+      },
+      runtimeChunk: true
+    }
+  })
+
+}
+
+module.exports = config;

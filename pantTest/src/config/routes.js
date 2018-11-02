b8329367e6bbb81d6235a1e499e8a5021fa94b6a
peggy <maggiegu94@163.com>
2018-11-02 11:09:18 +0800

    修改

--- pantTest/src/config/routes.js
+++ pantTest/src/config/routes.js
@@ -1,7 +1,35 @@
 import testRoutes from './test'
+import aestRoutes from './aest'
+import bestRoutes from './best'
 
 const Login = () => import('../views/login.vue');
 const Panel = () => import('../views/panel.vue');
+
+export const routeArr = [
+  {
+    path: 'test',
+    name: '测试T',
+    icon: 'clipboard',
+    component: resolve => require(['../layout/base.vue'], resolve),
+    children: testRoutes
+  },
+  {
+    path: 'aest',
+    name: '测试A',
+    icon: 'clipboard',
+    component: resolve => require(['../layout/base.vue'], resolve),
+    children: aestRoutes
+  },
+  {
+    path: 'best',
+    name: '测试B',
+    icon: 'clipboard',
+    component: resolve => require(['../layout/base.vue'], resolve),
+    children: bestRoutes
+  }
+]
+
+console.log(routeArr, ', ...routeArr--->', ...routeArr);
 
 export default [
   {
@@ -15,7 +43,6 @@ export default [
   {
     path: '/panel',
     component: Panel,
-    children: testRoutes
+    children: [...routeArr]
   }
-
 ]
--- pantTest/src/views/test/list.vue
+++ pantTest/src/views/test/list.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <Table :data="tableData" :columns="tableColumns"></Table>
+    <!-- <Table :data="tableData" :columns="tableColumns"></Table>
     <div style="margin: 10px;overflow: hidden">
         <div style="float: right;">
             <Page :total="100" :current="1" @on-change="changePage"></Page>
         </div>
-    </div>
+    </div> -->
   </div>
 </template>
 

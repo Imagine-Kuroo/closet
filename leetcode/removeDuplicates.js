11e1b791b77d4e8ecbba6e9f434e6bfa9bba551a
Kuroo <maggiegu94@163.com>
2019-03-06 21:21:13 +0800

    使用reduce会另外开辟数组空间...不符合题意

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -1,16 +1,28 @@
+// var removeDuplicates = function (nums) {
+//     if (nums.length == 0) return 0;
+//     let i = 0;
+//     for (let j = 1; j < nums.length; j++) {
+//         if (nums[j] != nums[i]) {
+//             i++;
+//             nums[i] = nums[j]
+//         }
+//     }
+//     return i + 1
+// };
+
 var removeDuplicates = function (nums) {
-    if (nums.length == 0) return 0;
-    let i = 0;
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j]
+    nums = nums.reduce((pre, cur) => {
+        if (!pre.includes(cur)) {
+            return pre.concat(cur)
+        } else {
+            return pre
         }
-    }
-    return i + 1
+    }, [])
+    console.log(nums)
+    return nums.length
 };
 
 
-let nums = [1,1,2]
+let nums = [1, 1, 2]
 let length = removeDuplicates(nums)
-console.log('arr.length--->',length)
+console.log('arr.length--->', length)
